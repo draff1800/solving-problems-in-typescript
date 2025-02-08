a3b6e488db38cd4640f2ae8d49e223cfff7212cb
draff1800 <draff1800@gmail.com>
2025-02-08 18:47:59 -0500

    (Quick) configuration to run jest tests from ./test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+
+export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": [
+      "ts-jest",
+      { 
+        useESM: true 
+      }
+    ],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 };
